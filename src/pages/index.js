9b5b850c7ec88aee9acd3bcbad4b2ece6a747ca5
0x63611e <96933161+naiba4@users.noreply.github.com>
2024-07-25 12:37:20 +0300

    Initialize add-new-docs

---
+++ src/pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/homepage-features/HomepageFeatures';
+import HomepageDeveloperLinks from '../components/homepage-developer-links/HomepageDeveloperLinks';
+import HomepagePreFooter from '../components/homepage-prefooter/HomepagePreFooter';
+import HomepageFooter from '../components/homepage-footer/HomepageFooter';
+import StartImage from '../../static/img/start-image.svg';
+import topBgImage from '../../static/img/topbg.svg';
+import footerGradientImage from '../../static/img/footer-gradient.svg';
+
+function HomepageHeader() {
+    return (
+        <header className={clsx('page-container', styles.headerWrap)}>
+            <h1 className={styles.mainTitle}>Documentation Portal</h1>
+            <div className={styles.searchWrap}>Search</div>
+        </header>
+    );
+}
+
+export default function Home() {
+    return (
+        <Layout pageClassName={styles.mainLayout}
+                title="1inch Network | Leading high capital efficient DeFi protocols"
+                description="The 1inch Network unites decentralized protocols whose synergy enables the most lucrative, fastest and protected operations in the DeFi space.">
+            {/*<StartImage className={styles.bgImage}/>*/}
+            <HomepageHeader/>
+            <main>
+                <div className={styles.section}>
+                    <HomepageFeatures/>
+                </div>
+                <div className={styles.section2}>
+                    <HomepageDeveloperLinks/>
+                </div>
+                <div className={styles.section3}>
+                    <HomepagePreFooter/>
+                </div>
+                <HomepageFooter/>
+            </main>
+            <img src={topBgImage} className={styles.topGradient} />
+            <img src={footerGradientImage} className={styles.bottomGradient} />
+        </Layout>
+    );
+}
