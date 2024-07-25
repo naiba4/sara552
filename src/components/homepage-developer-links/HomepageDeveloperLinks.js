f268cd6b8b195c4828a560e5bc65bd3de5127df9
0x63611e <96933161+naiba4@users.noreply.github.com>
2024-07-25 12:37:56 +0300

    Initialize l10n_new-branch3

---
+++ src/components/homepage-developer-links/HomepageDeveloperLinks.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styles from './HomepageDeveloperLinks.module.css';
+import Link from '@docusaurus/Link';
+import clsx from "clsx";
+
+const DeveloperList = [
+    {
+        link: 'https://1inch.io/foundation-grant-program/',
+        image: (
+            <div className={clsx(styles.developerCardImage, styles.foundationGrantProgramImage)}/>
+        ),
+        title: 'Grants program',
+        description: (
+            <>
+                Incentivizing individual developers and teams to build on the 1inch Network's protocols and contribute
+                to protocol evolution.
+            </>
+        ),
+    },
+    {
+        link: 'https://airtable.com/shrddQOOkm3hOLuzd',
+        image: (
+            <div className={clsx(styles.developerCardImage, styles.bugBountyImage)}/>
+        ),
+        title: 'Bug bounty',
+        description: (
+            <>
+                Engaging adopters and maximizing the network's decentralization through growing stakeowner numbers and
+                their participation
+            </>
+        ),
+    },
+];
+
+function DeveloperLink({image, title, description, link}) {
+    return link
+        ? (
+            <Link className={clsx(styles.developerCard, styles.developerCardLink)} to={link}>
+                <div>{image}</div>
+                <h3 className={styles.developerCardTitle}>{title}</h3>
+                <div className={styles.developerCardText}>{description}</div>
+            </Link>
+        )
+        : (
+            <div className={styles.developerCard}>
+                <div>{image}</div>
+                <h3 className={styles.developerCardTitle}>{title}</h3>
+                <div className={styles.developerCardText}>{description}</div>
+            </div>
+        );
+}
+
+export default function HomepageDeveloperLinks() {
+    return (
+        <section>
+            <h2 className={styles.developerLinksTitle}>Developer links</h2>
+            <div className={styles.developerLinksWrap}>
+                {
+                    DeveloperList.map((props, idx) => (
+                        <DeveloperLink key={idx} {...props} />
+                    ))
+                }
+            </div>
+        </section>
+    );
+}
